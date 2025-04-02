6d3be3a129ed41e9f75ca6e194622f8ee7fab23c
Pedro Lima <pedrohglima01@gmail.com>
2025-04-02 12:06:28 -0300

    Atualiza .gitignore para incluir arquivos PNG e otimiza o script de benchmark com barra de progresso e tratamento de erros

--- analise/benchmark.py
+++ analise/benchmark.py
@@ -1,8 +1,9 @@
 import os
 import pandas as pd
 from tqdm import tqdm
+from itertools import product
 
-n_sizes = [16, 32, 64, 128, 256, 512, 1024, 2048]
+n_sizes = [2**n for n in range(4, 14)]
 n_repeats = 10
 
 
@@ -12,32 +13,35 @@ def main():
     os.makedirs(f"{os.path.dirname(__file__)}/benchmark", exist_ok=True)
     
     dados = []
-    for n in tqdm(n_sizes):
-        for i in tqdm(range(n_repeats), leave=False):
-            # rodar o programa
-            arquivo = f"{os.path.dirname(__file__)}/benchmark/run_{n}_{i:02d}.txt"
-            cmd = f"cd {path_to_bins} && ./dgemm.exe {n} > {arquivo}"
-            # print(cmd)
-            os.system(cmd)
+    pbar = tqdm(product(n_sizes, range(n_repeats)), total=(len(n_sizes) * n_repeats))
+    for n, i in pbar:
+        pbar.set_description(f"{n=} {i=}")
+        # rodar o programa
+        arquivo = f"{os.path.dirname(__file__)}/benchmark/run_{n}_{i:02d}.txt"
+        cmd = f"{path_to_bins}/dgemm.exe {n} > {arquivo}"
+        # print(cmd)
+        c = os.system(cmd)
+        if c != 0:
+            print(f"Interrompendo benchmark, erro: {c}")
+            break
 
-            # pegar o resultado
-            with open(arquivo, "r") as f:
-                lines = f.readlines()[5:]
-                for line in lines:
-                    metodo = line.split(' ')[0]
-                    tempo = float(line.split(' ')[1]) # ms
-                    
-                    dados.append(
-                        {
-                            "n": n,
-                            "run": i,
-                            "metodo": metodo,
-                            "tempo": tempo
-                        }
-                    )
+        # pegar o resultado
+        with open(arquivo, "r") as f:
+            lines = f.readlines()[5:] # pular para o resultado
+            for line in lines:
+                metodo = line.split(' ')[0]
+                tempo = float(line.split(' ')[1]) # ms
+                
+                dados.append(
+                    {
+                        "n": n,
+                        "run": i,
+                        "metodo": metodo,
+                        "tempo": tempo
+                    }
+                )
     
     df = pd.DataFrame(dados)
-    print(df.head())
     df.to_csv(f"{os.path.dirname(__file__)}/benchmark/benchmark.csv", index=False)
 
 
