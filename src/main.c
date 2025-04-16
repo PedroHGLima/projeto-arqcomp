b1bea79c1e4d14baf3419f1fd3e17d6de7689cc3
Pedro Lima <pedrohglima01@gmail.com>
2025-04-16 15:02:31 -0300

    Melhor modularidade para testes na main

--- src/main.c
+++ src/main.c
@@ -4,79 +4,46 @@ int main(int argc, char* argv[]) {
     srand(SEED);
     size_t n, n2;
     get_args(argc, argv, &n);
-    if (n == 0 || n%4 != 0) {
-        fprintf(stderr, "Tamanho da matriz deve ser maior que 0 e multiplo de 4\n");
+    if ((n == 0) || (n%16 != 0)) {
+        fprintf(stderr, "Tamanho da matriz deve ser maior que 0 e multiplo de 16\n");
         return 1;
     }
     n2 = n * n;
     printf("Tamanho da matriz:\nN=%lu\nN2=%lu\nMEM=%.2f MB\n", n, n2, n2*sizeof(double)/1e6);
 
-    double *A, *B, *C;	// declarando assim para garantir memoria alinhada
+    double *A, *B, *Ref;	// declarando assim para garantir memoria alinhada
     A = (double*) aligned_alloc(4*sizeof(double), n2 * sizeof(double));
     B = (double*) aligned_alloc(4*sizeof(double), n2 * sizeof(double));
-    C = (double*) aligned_alloc(4*sizeof(double), n2 * sizeof(double));
+    Ref = (double*) aligned_alloc(4*sizeof(double), n2 * sizeof(double));
     for (size_t i = 0; i < n2; i++) {
         A[i] = (double)rand() / RAND_MAX;
         B[i] = (double)rand() / RAND_MAX;
-        C[i] = 0.0;
+        Ref[i] = 0.0;
     }
-    if (A == NULL || B == NULL || C == NULL) {
+    if (A == NULL || B == NULL || Ref == NULL) {
         fprintf(stderr, "Erro ao alocar memoria\n");
         return 1;
     }
 
-    clock_t start, end;
     double time_taken;
+    dgemm(n, A, B, Ref);
     printf("Tempo de execucao: \n");
     
     // Testar a funcao dgemm original
-    zerar_matriz(C, n);
-    start = clock();
-    dgemm(n, A, B, C);
-    end = clock();
-    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
-    printf("DGEMM %.3f ms\n", time_taken);
-    // Printar a matriz C
-    for (size_t i = 0; i < n; i++) {
-    	for (size_t j = 0; j < n; j++) {
-    		printf("%lf ", C[i * n + j]);
-    	}
-    	printf("\n");
-    }
+    time_taken = multiplicar(n, A, B, Ref, dgemm);
+    if (time_taken!=-1) printf("DGEMM %.3f ms\n", time_taken);
 
     // Testar a funcao dgemm otimizada com AVX
-    zerar_matriz(C, n);
-    start = clock();
-    avx_dgemm(n, A, B, C);
-    end = clock();
-    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
-    printf("AVX %.3f ms\n", time_taken);
-    // Printar a matriz C
-    for (size_t i = 0; i < n; i++) {
-    	for (size_t j = 0; j < n; j++) {
-    		printf("%lf ", C[i * n + j]);
-    	}
-    	printf("\n");
-    }
+    time_taken = multiplicar(n, A, B, Ref, avx_dgemm);
+    if (time_taken!=-1) printf("AVX %.3f ms\n", time_taken);
 
     // Testar a funcao dgemm otimizada com AVX e loop unrolling
-    zerar_matriz(C, n);
-    start = clock();
-    loop_unr(n, A, B, C);
-    end = clock();
-    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
-    printf("LOOP_UNROLLING %.3f ms\n", time_taken);
-    // Printar a matriz C
-    for (size_t i = 0; i < n; i++) {
-    	for (size_t j = 0; j < n; j++) {
-    		printf("%lf ", C[i * n + j]);
-    	}
-    	printf("\n");
-    }
+    time_taken = multiplicar(n, A, B, Ref, loop_unr);
+    if (time_taken!=-1) printf("AVX+LU %.3f ms\n", time_taken);
 
     free(A);
     free(B);
-    free(C);
+    free(Ref);
 
     return 0;
 }
