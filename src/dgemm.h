900ce7fbed57091f0f7f5134e2a5ab56cf36bdfb
PedroHGLima <pedrohglima01@gmail.com>
2025-04-29 10:43:49 -0300

    Implementaçáo da otimização de cache blocking

--- src/dgemm.h
+++ src/dgemm.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <x86intrin.h>
 
-#define UNROLL 4
+#define UNROLL (4)
+#define BLOCKSIZE 32
 
 /**
  * @brief Funcao DGEMM original
@@ -32,7 +33,7 @@ void avx_dgemm (size_t n, double* A, double* B, double* C);
  * @brief DGEMM otimizada usando loop unrolling,
  *  criando mais oportunidades para paralelismo a nível de instrução
  * pg. 651
- * fig 3.22
+ * fig 4.77
  * 
  * @param n Tamanho da matriz (n x n)
  * @param A Operando A
@@ -40,3 +41,16 @@ void avx_dgemm (size_t n, double* A, double* B, double* C);
  * @param C Resultado
  */
 void loop_unr (size_t n, double* A, double* B, double* C);
+
+/**
+ * @brief DGEMM otimizada usando cache blocking
+ * pg. 923
+ * fig 5.47
+ * 
+ * @param n Tamanho da matriz (n x n)
+ * @param A Operando A
+ * @param B Operando B
+ * @param C Resultado
+ */
+void cache_blocking(size_t n, double* A, double* B, double* C);
+void do_block(int n, int si, int sj, int sk, double *A, double *B, double *C);
